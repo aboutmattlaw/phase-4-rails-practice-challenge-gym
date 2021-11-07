e8128fb94c8ae21673477b2764907d6b2430f5fe
Matt <aboutmattlaw@gmail.com>
2021-11-07 17:19:11 -0500

    commit tree ce6c9f6e22dfd98f4c657a0aa29736517ca90100

---
+++ app/controllers/gyms_controller.rb
@@ -0,0 +1,57 @@
+class GymsController < ApplicationController
+
+
+  before_action :set_gym, only: [:show, :update, :destroy]
+
+  # GET /gyms
+  def index
+    @gyms = Gym.all
+
+    render json: @gyms
+  end
+
+  # GET /gyms/1
+  def show
+    if @gym
+    render json: @gym
+    else
+      render json: { error: "not found"}, status: :unprocessable_entity
+    end
+  end
+
+  # POST /gyms
+  def create
+    @gym = Gym.new(gym_params)
+
+    if @gym.save
+      render json: @gym, status: :created, location: @gym
+    else
+      render json: @gym.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /gyms/1
+  def update
+    if @gym.update(gym_params)
+      render json: @gym
+    else
+      render json: @gym.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /gyms/1
+  def destroy
+    @gym.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_gym
+      @gym = Gym.find_by(id: params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def gym_params
+      params.require(:gym).permit(:name, :address)
+    end
+end
